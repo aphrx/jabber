067029bf56a31f4245e615440b834c867a9d75ee
aphrx <amalnnath20@gmail.com>
2020-02-05 19:56:45 -0500

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,42 @@
+import requests
+import bs4
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+
+URL = "https://ca.indeed.com/new-grad-software-jobs-in-Kitchener"
+
+page =  requests.get(URL)
+
+soup = BeautifulSoup(page.text, "html.parser")
+print(soup)
+jobs = []
+jobCount = 0
+employer = []
+employerCount = 0
+
+def job_title(soup):
+	global jobCount, employerCount
+	for div in soup.find_all(name="div", attrs={"class":"row"}):
+		for a in div.find_all(name="a", attrs={"data-tn-element": "jobTitle"}):
+			jobs.append(a["title"])
+			jobCount = jobCount + 1
+		company = div.find_all(name="span", attrs={"class":"company"})
+		if len(company) > 0:
+			for b in company:
+				employer.append(b.text.strip())
+				employerCount = employerCount + 1
+		else:
+			sec_try = div.find_all(name="span", attrs={"class":"result-link-source"})
+			for span in sec_try:
+				employer.append(span.text.strip())
+				employerCount = employerCount + 1
+	return(jobs)
+
+
+job_title(soup)
+print(jobs)
+print(employer)
+
+print("Total Jobs:" + str(jobCount))
+print("Total Employers:" + str(employerCount))

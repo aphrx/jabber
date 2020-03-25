1687987a2963b7cad851d1a956fef57d5d9e8999
Aphrx <amalnnath20@yahoo.ca>
2020-03-25 12:43:44 -0400

    run file

---
+++ run.py
@@ -0,0 +1,22 @@
+import scraper
+import linkedin_apply
+
+test = scraper.scrape()
+
+jobs = []
+employer = []
+links = []
+selenium = []
+
+jobs, employer, links, count = test.search("software developer", "toronto", True)
+
+
+for i in range(0, len(jobs)):
+	print(jobs[i])
+	print(employer[i])
+	print(links[i])
+	print(" ")
+
+for j in range(0, len(links)):
+	jobs[j] = linkedin_apply.apply("amalnnath20@gmail.com", "Pikachu20s", links[j])
+	jobs[j].run()

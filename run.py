23bab60af9c7f3022b3d51860104e618895acf21
Aphrx <amalnnath20@yahoo.ca>
2020-03-25 13:16:10 -0400

    selenium test

--- run.py
+++ run.py
@@ -17,6 +17,6 @@ for i in range(0, len(jobs)):
 	print(links[i])
 	print(" ")
 
-for j in range(0, len(links)):
-	jobs[j] = linkedin_apply.apply("amalnnath20@gmail.com", "Pikachu20s", links[j])
-	jobs[j].run()
+#for j in range(0, len(links)):
+#	jobs[j] = linkedin_apply.apply("amalnnath20@gmail.com", "Pikachu20s", links[j])
+#	jobs[j].run()
